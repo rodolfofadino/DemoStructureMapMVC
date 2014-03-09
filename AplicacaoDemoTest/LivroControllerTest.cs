c9a052899195329278675f82fed869dc488413b9
rodolfofadino <rodolfo.fadino@gmail.com>
2014-03-09 19:45:37 -0300

    criação do projeto de teste

---
+++ AplicacaoDemoTest/LivroControllerTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AplicacaoDemoTest
+{
+    [TestClass]
+    public class LivroControllerTest
+    {
+       
+    }
+}
