76b839873755c73007ea01db5fe84b874a8eef1b
rodolfofadino <rodolfo.fadino@gmail.com>
2014-03-09 19:12:49 -0300

    criação do projeot

---
+++ AplicacaoDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AplicacaoDemo.MvcApplication" Language="C#" %>
---
+++ AplicacaoDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AplicacaoDemo.Startup))]
+namespace AplicacaoDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
