cda86e0635e09e81986f7a2082f70a654dab2aea
rodolfofadino <rodolfo.fadino@gmail.com>
2014-03-09 19:58:36 -0300

    criacao do teste da regra quando nao tem livro

--- AplicacaoDemoTest/LivroControllerTest.cs
+++ AplicacaoDemoTest/LivroControllerTest.cs
@@ -1,11 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using AplicacaoDemo.Models;
+using AplicacaoDemo.Repository;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
 
 namespace AplicacaoDemoTest
 {
     [TestClass]
     public class LivroControllerTest
     {
-       
+        [TestMethod]
+        public void ValidaARegraQuandoNaoTemLivro()
+        {
+            var repositoryFake = new LivroRepositoryFake();
+            var controller = new AplicacaoDemo.Controllers.LivroController(repositoryFake);
+            var result = controller.Index() as ViewResult;
+
+            Assert.AreEqual("SemLivros", result.ViewName);
+        }
+
     }
+
+    public class LivroRepositoryFake : ILivroRepository
+    {
+        public List<Livro> GetAll()
+        {
+            return new List<Livro>();
+        }
+    }
+
 }
