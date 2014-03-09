d3c738ac030ba606d74c69a374c9bcdbbfa9b742
rodolfofadino <rodolfo.fadino@gmail.com>
2014-03-09 19:37:24 -0300

    aplicação funcionando com a lista de livros

---
+++ AplicacaoDemo/Controllers/LivroController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AplicacaoDemo.Controllers
+{
+    public class LivroController : Controller
+    {
+        public ActionResult Index()
+        {
+            var repository = new Repository.LivroRepository();
+            var livros = repository.GetAll();
+
+            return View(livros);
+        }
+	}
+}
---
+++ AplicacaoDemo/Repository/LivroRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using AplicacaoDemo.Models;
+
+namespace AplicacaoDemo.Repository
+{
+    public class LivroRepository
+    {
+        public List<Livro> GetAll()
+        {
+            var retorno=new List<Livro>
+            {
+                new Livro() {Id = 1, Titulo = "Madame Bovay", Autor = "Gustave Flaubert"},
+                new Livro() {Id = 2, Titulo = "Guerra e Paz", Autor = "Leon Tolstoi"},
+                new Livro() {Id = 3, Titulo = "Romeo and Juliet", Autor = "William Shakespeare"},
+                new Livro() {Id = 4, Titulo = "O efeito sombra", Autor = "Marianne Williamson"},
+                new Livro() {Id = 5,Titulo = "Professional ASP.NET MVC 4",Autor = "Galloway, Jon, Haack, Phil, Wilson, Brad and Allen"}
+            };
+
+            return retorno;
+        }
+    }
+}
